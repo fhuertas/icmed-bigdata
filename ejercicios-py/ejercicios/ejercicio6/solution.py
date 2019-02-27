8ee9e22daafe814341f1a9a38dcdbd9f34721447
fhuertas <francisco@fhuertas.com>
2019-02-27 20:08:26 +0100

    Update example

--- ejercicios-py/ejercicios/ejercicio6/solution.py
+++ ejercicios-py/ejercicios/ejercicio6/solution.py
@@ -8,24 +8,38 @@ def job():
         .builder \
         .appName("KafkaTweetsReader").getOrCreate()
 
-    schema = StructType([
+    schema_messages = StructType([
         StructField("id", StringType(), True),
         StructField("createdAt", StringType(), True)])
+
+    schema_users = StructType([StructField("id", StringType(), True), StructField("name", StringType(), True)])
 
     kafka_input = spark \
         .readStream \
         .format("kafka") \
-        .option("kafka.bootstrap.servers", "broker-1:9092,broker-2:9092,broker-3:9092") \
+        .option("kafka.bootstrap.servers", "localhost:19092,localhost:29092,localhost:39092") \
         .option("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer") \
         .option("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer") \
-        .option("subscribe", "tweets-out") \
+        .option("subscribe", "tweets") \
         .load()
 
-    raw_input = kafka_input.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
-        .withColumn("json", from_json('value', schema)) \
-        .withColumn("id", col('json.id'))
+    users = spark.read.format("format") \
+        .option("kafka.bootstrap.servers", "localhost:19092,localhost:29092,localhost:39092") \
+        .option("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer") \
+        .option("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer") \
+        .option("subscribe", "tweets") \
+        .load() \
+        .withColumn("users_json", from_json('value', schema_users)) \
+        .withColumn("id", col('users_json.id'))
 
-    query = raw_input \
+    tweets = kafka_input.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
+        .withColumn("msg_json", from_json('value', schema_messages)) \
+        .withColumn("user_id", col('msg_json.userId'))
+
+    join_df = users.join(tweets, exp("user_id = id"))
+
+    # query = raw_input.writeStream.format("json").option("path", "/tmp/json-dir").start()
+    query = tweets \
         .writeStream \
         .outputMode("append") \
         .format("console") \
