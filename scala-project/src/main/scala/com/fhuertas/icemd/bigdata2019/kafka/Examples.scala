7912fbd1ad7debad1295be67e16280af362553f4
fhuertas <francisco@fhuertas.com>
2019-03-01 19:57:52 +0100

    Flujo consumidor productor

--- scala-project/src/main/scala/com/fhuertas/icemd/bigdata2019/kafka/Examples.scala
+++ scala-project/src/main/scala/com/fhuertas/icemd/bigdata2019/kafka/Examples.scala
@@ -3,7 +3,7 @@ import java.util.Properties
 
 import com.fhuertas.icemd.bigdata2019.dsl.StringDsl._
 import org.apache.kafka.clients.consumer.KafkaConsumer
-import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.kafka.clients.producer.{ KafkaProducer, ProducerRecord }
 
 import scala.collection.JavaConverters._
 import scala.compat.java8.DurationConverters._
@@ -12,19 +12,31 @@ import scala.util.Random
 
 object Examples extends App {
 
-  def consumerExample(): Unit = {
+  def consumerExample(origen: String, destino: String): Unit = {
     val consumerProperties = new Properties()
-    consumerProperties.setProperty("bootstrap.servers", "localhost:19092")
+    consumerProperties.setProperty("bootstrap.servers",
+                                   "follower-1:6667,follower-2:6667,follower-3:6667,leader-1:6667,leader-2:6667")
     consumerProperties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
     consumerProperties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
     consumerProperties.setProperty("group.id", s"${System.currentTimeMillis}")
+
+    val producerProperties = new Properties()
+    producerProperties.setProperty("bootstrap.servers", "follower-1:6667,follower-2:6667,follower-3:6667,leader-1:6667,leader-2:6667")
+    producerProperties.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    producerProperties.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    val producer = new KafkaProducer[String, String](producerProperties)
+
     val consumer = new KafkaConsumer[Nothing, String](consumerProperties)
 
-    consumer.subscribe(Seq("topic-origen").asJava)
+    consumer.subscribe(Seq(origen).asJava)
 
     while (true) {
       val records = consumer.poll(100.millis.toJava)
-      records.asScala.foreach(record ⇒ println(wordCount(record.value)))
+      records.asScala.foreach(recordOrigin ⇒ {
+        val words = wordCount(recordOrigin.value)
+        val recordDestino = new ProducerRecord(destino, words.toString, s"$words -> ${recordOrigin.value}")
+        producer.send(recordDestino)
+      })
     }
   }
 
@@ -42,6 +54,6 @@ object Examples extends App {
     }
   }
 
-  consumerExample()
+  consumerExample(args(0),args(1))
 
 }
